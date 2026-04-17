1d8fcbef5b384e67c631680d56d70ce131e0d7b4
Renato T. Bonnin <154079188+RenatoBonnin@users.noreply.github.com>
2026-04-16 22:47:07 -0300

    Update Ahorcado.py

--- Ahorcado.py
+++ Ahorcado.py
@@ -23,155 +23,154 @@ def leerArchivo():
 #FUNCIONES PARA LA PRIMERA PARTE DE DEF JUEGO(). ASIGNAN UNA PALABRA RANDOM DE LA LISTA
 
 def asignarPalabraAdiv(palabra):
-	palabraAdiv = ""
-	for letra in palabra:
-		palabraAdiv = palabraAdiv + "_"
-	return palabraAdiv
+        palabraAdiv = ""
+        for letra in palabra:
+                palabraAdiv = palabraAdiv + "_"
+        return palabraAdiv
 
 def asignarPalabraRandom(listaPalabras):
-	palabra = random.choice(listaPalabras)
-	return palabra
+        palabra = random.choice(listaPalabras)
+        return palabra
 
 #INICIALIZACIÓN DEL JUEGO Y BUCLE PRINCIPAL
 
 def juego(listaPalabras):
-	vic = False
-	contErrores = 7
-	palabra = asignarPalabraRandom(listaPalabras)
-	palabraAdiv = asignarPalabraAdiv(palabra)
-	letrasUsadas = []
+        vic = False
+        contErrores = 7
+        palabra = asignarPalabraRandom(listaPalabras)
+        palabraAdiv = asignarPalabraAdiv(palabra)
+        letrasUsadas = []
 
-	while not vic and contErrores > 0:
-		
-		mostrarCiclo(palabraAdiv, contErrores)
+        while not vic and contErrores > 0:
 
-		letrasUsadas.sort()
+                mostrarCiclo(palabraAdiv, contErrores)
 
-		listarLetrasUsadas(letrasUsadas)
+                letrasUsadas.sort()
 
-		letraAdiv = input("INGRESE UNA LETRA: ").upper()
-		
-		if letraAdiv not in letrasUsadas:
-			
-			letrasUsadas.append(letraAdiv)
+                listarLetrasUsadas(letrasUsadas)
 
-			palabraAdiv = list(palabraAdiv)
-			
-			error = letraALetra(palabra, palabraAdiv, letraAdiv)
-			
-			contErrores = siErrores(error, contErrores)
-				
-			palabraAdiv = listaToString(palabraAdiv)	
-		
-			vic = verificarEstado(palabra, palabraAdiv, contErrores)
-		
-		else: 
-			print("LETRA YA USADA")
+                letraAdiv = input("INGRESE UNA LETRA: ").upper()
+
+                if letraAdiv not in letrasUsadas:
+
+                        letrasUsadas.append(letraAdiv)
+
+                        palabraAdiv = list(palabraAdiv)
+
+                        error = letraALetra(palabra, palabraAdiv, letraAdiv)
+
+                        contErrores = siErrores(error, contErrores)
+
+                        palabraAdiv = listaToString(palabraAdiv)        
+
+                        vic = verificarEstado(palabra, palabraAdiv, contErrores)
+
+                else: 
+                        print("LETRA YA USADA")
 
 def juegoUI(listaPalabras):
-	pass
+        pass
 
 #FUNCIONES USADAS DENTRO DEL BUCLE PRINCIPAL
 
 def mostrarPalabraAdiv(palabraAdiv):
-	print("PALABRA A ADIVINAR: ", palabraAdiv)
-	
+        print("PALABRA A ADIVINAR: ", palabraAdiv)
+
 def mostrarErrores(contErrores):
-	print("ERRORES RESTANTES: ", contErrores)
-	
+        print("ERRORES RESTANTES: ", contErrores)
+
 def mostrarCiclo(palabraAdiv, contErrores):
-	mostrarPalabraAdiv(palabraAdiv)
-	mostrarErrores(contErrores)
+        mostrarPalabraAdiv(palabraAdiv)
+        mostrarErrores(contErrores)
 
 def listarLetrasUsadas(letrasUsadas):
-	print(*letrasUsadas)
+        print(*letrasUsadas)
 
 def letraALetra(palabra, palabraAdiv, letraAdiv):
-	error = True
-	i = 0
-	for letra in palabra:
-		if letraAdiv == letra:
-			error = False
-			palabraAdiv[i]= letra
-		i += 1
-	return error
+        error = True
+        i = 0
+        for letra in palabra:
+                if letraAdiv == letra:
+                        error = False
+                        palabraAdiv[i]= letra
+                i += 1
+        return error
 
 def listaToString(string):
-	return "".join(string)
+        return "".join(string)
 
 def siErrores(error, contErrores):
-	if error:
-		contErrores -= 1
-	return contErrores
+        if error:
+                contErrores -= 1
+        return contErrores
 
 def condVictoria(palabra, palabraAdiv):
-	if palabra == palabraAdiv:
-		print("GANASTE!!! :)")
-		mostrarPalabra(palabra)
-		input()
-		return True
+        if palabra == palabraAdiv:
+                print("GANASTE!!! :)")
+                mostrarPalabra(palabra)
+                return True
 
 def condMuerte(contErrores, palabra):
-	if contErrores <= 0:
-		print("PERDISTE :(")
-		mostrarPalabra(palabra)
-		input()
-	
+        if contErrores <= 0:
+                print("PERDISTE :(")
+                mostrarPalabra(palabra)
+                input()
+
 def mostrarPalabra(palabra):
-	print("LA PALABRA ERA: ", palabra)
-								
+        print("LA PALABRA ERA: ", palabra)
+
 def verificarEstado(palabra, palabraAdiv, contErrores):
-	condMuerte(contErrores, palabra)
-	return condVictoria(palabra, palabraAdiv)
+        condMuerte(contErrores, palabra)
+        return condVictoria(palabra, palabraAdiv)
 
 #MENU DE CONFIGURACIÓN
 
 def config(listaPalabras):
-	
-	op = 8
-	
-	while op != "0":
-		print("0 - SALIR")
-		print("1 - LISTAR PALABRAS")
-		print("2 - AGREGAR PALABRA")
-		print("3 - ELIMINAR PALABRA")
-		op = input("SELECCIONE UNA OPCION: ")
-		match op:
-			case "1":
-				listarPalabras(listaPalabras)
-			case "2":
-				agregarPalabra(listaPalabras)
-			case "3":
-				if listaPalabras:
-					eliminarPalabra(listaPalabras)
-				else:
-					print("LISTA VACIA")
-			case _:
-				if op != "0":
-					print("OPCION INVALIDA")
-	
-	modificarArchivo(listaPalabras)
-	
-#FUNCIONES PARA CONFIG	
-	
+
+        op = 8
+
+        while op != "0":
+                print("0 - SALIR")
+                print("1 - LISTAR PALABRAS")
+                print("2 - AGREGAR PALABRA")
+                print("3 - ELIMINAR PALABRA")
+                op = input("SELECCIONE UNA OPCION: ")
+                match op:
+                        case "1":
+                                listarPalabras(listaPalabras)
+                        case "2":
+                                agregarPalabra(listaPalabras)
+                        case "3":
+                                if listaPalabras:
+                                        eliminarPalabra(listaPalabras)
+                                else:
+                                        print("LISTA VACIA")
+                        case _:
+                                if op != "0":
+                                        print("OPCION INVALIDA")
+
+        modificarArchivo(listaPalabras)
+
+#FUNCIONES PARA CONFIG        
+
 def listarPalabras(listaPalabras):
-	print(*listaPalabras)
+        print(*listaPalabras)
 
 def listarPalabrasUI(listaPalabras):
-	texto = ""
-	i = 1
-	for palabra in listaPalabras:
-		
-		if len(listaPalabras) == i:
-			texto += f"{palabra}"
-		else:
-			texto += f"{palabra}, "
-		i += 1
-	return texto
+        texto = ""
+        i = 1
+        for palabra in listaPalabras:
+
+                if len(listaPalabras) == i:
+                        texto += f"{palabra}"
+                else:
+                        texto += f"{palabra}, "
+                i += 1
+        return texto
 
 def agregarPalabra(listaPalabras):
-	nuevaPalabra = input("NUEVA PALABRA: ").upper()
-	listaPalabras.append(nuevaPalabra)
+        nuevaPalabra = input("NUEVA PALABRA: ").upper()
+        listaPalabras.append(nuevaPalabra)
 
 def limpiar_espacios(texto):
     # strip() elimina espacios al inicio y final de la cadena
@@ -180,49 +179,49 @@ def limpiar_espacios(texto):
     return " ".join(texto.strip().split())
 
 def esPalabraValida(palabra):
-	for letra in palabra:
-		if not (letra.isalpha() or letra.isspace()):
-			return False
-	return True
-		
+        for letra in palabra:
+                if not (letra.isalpha() or letra.isspace()):
+                        return False
+        return True
+
 def esPalabraRepetida(listaPalabras, palabraBuscada):
-	for palabra in listaPalabras:
-		if palabra == palabraBuscada:
-			return True
-	return False
+        for palabra in listaPalabras:
+                if palabra == palabraBuscada:
+                        return True
+        return False
 
 def agregarPalabraUI(listaPalabras, palabraNueva):
-	palabraNueva = limpiar_espacios(palabraNueva)
+        palabraNueva = limpiar_espacios(palabraNueva)
 
-	palabraNueva = palabraNueva.upper()
+        palabraNueva = palabraNueva.upper()
 
-	repetida = esPalabraRepetida(listaPalabras, palabraNueva); valida = esPalabraValida(palabraNueva)
+        repetida = esPalabraRepetida(listaPalabras, palabraNueva); valida = esPalabraValida(palabraNueva)
 
-	if len(palabraNueva) > 1 and valida and not repetida:
-		
-		listaPalabras.append(palabraNueva)
-		return (True,not valida, repetida)
-	elif not esPalabraValida(palabraNueva):
-		return (False, not valida, repetida)
-	elif esPalabraRepetida(listaPalabras, palabraNueva):	
-		return (False, not valida, repetida)
-	else: return(False, False, False)
+        if len(palabraNueva) > 1 and valida and not repetida:
+
+                listaPalabras.append(palabraNueva)
+                return (True,not valida, repetida)
+        elif not esPalabraValida(palabraNueva):
+                return (False, not valida, repetida)
+        elif esPalabraRepetida(listaPalabras, palabraNueva):        
+                return (False, not valida, repetida)
+        else: return(False, False, False)
 
 def eliminarPalabra(listaPalabras):
-	palabra = input("INGRESE PALABRA A ELIMINAR: ")
-	if palabra in listaPalabras:
-		listaPalabras.remove(palabra)
-	else:
-		input("PALABRA NO ENCONTRADA")
+        palabra = input("INGRESE PALABRA A ELIMINAR: ")
+        if palabra in listaPalabras:
+                listaPalabras.remove(palabra)
+        else:
+                input("PALABRA NO ENCONTRADA")
 
 def eliminarPalabraUI(listaPalabras, palabraEliminar):
-	limpiar_espacios(palabraEliminar)
-	palabraEliminar = palabraEliminar.upper()
-	if palabraEliminar in listaPalabras:
-		listaPalabras.remove(palabraEliminar)
-		return True
-	else:
-		return False
+        limpiar_espacios(palabraEliminar)
+        palabraEliminar = palabraEliminar.upper()
+        if palabraEliminar in listaPalabras:
+                listaPalabras.remove(palabraEliminar)
+                return True
+        else:
+                return False
 
 #CUERPO PRINCIPAL
 """
@@ -232,19 +231,19 @@ listaPalabras = leerArchivo()
 op =8
 
 while op != "0":
-	print("0 - SALIR")
-	print("1 - JUGAR")
-	print("2 - CONFIGURACION")
-	op = input("SELECCIONE UNA OPCION: ")
-	match op:
-		case "1":
-			if listaPalabras:
-				juego(listaPalabras)
-			else:
-				print("LISTA VACIA")
-		case "2":
-			config(listaPalabras)
-		case _:
-			if op != "0":
-				print("OPCION INVALIDA")
+        print("0 - SALIR")
+        print("1 - JUGAR")
+        print("2 - CONFIGURACION")
+        op = input("SELECCIONE UNA OPCION: ")
+        match op:
+                case "1":
+                        if listaPalabras:
+                                juego(listaPalabras)
+                        else:
+                                print("LISTA VACIA")
+                case "2":
+                        config(listaPalabras)
+                case _:
+                        if op != "0":
+                                print("OPCION INVALIDA")
 """
